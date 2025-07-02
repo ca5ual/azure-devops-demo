d1816690355c67a1281b2c5fff24dd078fcc3581
Denis Zhak <deniszhak4@gmail.com>
2025-07-02 14:16:19 +0200

    change homepage text to test CI/CD

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello from Azure DevOps project"
+	return "CI/CD pipeline is work and everything is running"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
